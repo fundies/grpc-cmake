cdf8a3ec9f36ea08239b2819d35d1629e51e05aa
Greg Williamson <cheeseboy16@gmail.com>
2020-02-16 02:47:07 +0000

    first commit

---
+++ gRPCTargets-release.cmake
@@ -0,0 +1,302 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "gRPC::absl_bad_optional_access" for configuration "Release"
+set_property(TARGET gRPC::absl_bad_optional_access APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::absl_bad_optional_access PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libabsl_bad_optional_access.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::absl_bad_optional_access )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::absl_bad_optional_access "${_IMPORT_PREFIX}/lib/libabsl_bad_optional_access.a" )
+
+# Import target "gRPC::absl_base" for configuration "Release"
+set_property(TARGET gRPC::absl_base APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::absl_base PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libabsl_base.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::absl_base )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::absl_base "${_IMPORT_PREFIX}/lib/libabsl_base.a" )
+
+# Import target "gRPC::absl_dynamic_annotations" for configuration "Release"
+set_property(TARGET gRPC::absl_dynamic_annotations APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::absl_dynamic_annotations PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libabsl_dynamic_annotations.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::absl_dynamic_annotations )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::absl_dynamic_annotations "${_IMPORT_PREFIX}/lib/libabsl_dynamic_annotations.a" )
+
+# Import target "gRPC::absl_int128" for configuration "Release"
+set_property(TARGET gRPC::absl_int128 APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::absl_int128 PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libabsl_int128.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::absl_int128 )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::absl_int128 "${_IMPORT_PREFIX}/lib/libabsl_int128.a" )
+
+# Import target "gRPC::absl_log_severity" for configuration "Release"
+set_property(TARGET gRPC::absl_log_severity APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::absl_log_severity PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libabsl_log_severity.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::absl_log_severity )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::absl_log_severity "${_IMPORT_PREFIX}/lib/libabsl_log_severity.a" )
+
+# Import target "gRPC::absl_raw_logging_internal" for configuration "Release"
+set_property(TARGET gRPC::absl_raw_logging_internal APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::absl_raw_logging_internal PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libabsl_raw_logging_internal.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::absl_raw_logging_internal )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::absl_raw_logging_internal "${_IMPORT_PREFIX}/lib/libabsl_raw_logging_internal.a" )
+
+# Import target "gRPC::absl_spinlock_wait" for configuration "Release"
+set_property(TARGET gRPC::absl_spinlock_wait APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::absl_spinlock_wait PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libabsl_spinlock_wait.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::absl_spinlock_wait )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::absl_spinlock_wait "${_IMPORT_PREFIX}/lib/libabsl_spinlock_wait.a" )
+
+# Import target "gRPC::absl_strings" for configuration "Release"
+set_property(TARGET gRPC::absl_strings APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::absl_strings PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libabsl_strings.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::absl_strings )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::absl_strings "${_IMPORT_PREFIX}/lib/libabsl_strings.a" )
+
+# Import target "gRPC::absl_strings_internal" for configuration "Release"
+set_property(TARGET gRPC::absl_strings_internal APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::absl_strings_internal PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libabsl_strings_internal.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::absl_strings_internal )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::absl_strings_internal "${_IMPORT_PREFIX}/lib/libabsl_strings_internal.a" )
+
+# Import target "gRPC::absl_throw_delegate" for configuration "Release"
+set_property(TARGET gRPC::absl_throw_delegate APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::absl_throw_delegate PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libabsl_throw_delegate.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::absl_throw_delegate )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::absl_throw_delegate "${_IMPORT_PREFIX}/lib/libabsl_throw_delegate.a" )
+
+# Import target "gRPC::address_sorting" for configuration "Release"
+set_property(TARGET gRPC::address_sorting APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::address_sorting PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libaddress_sorting.so.9.0.0"
+  IMPORTED_SONAME_RELEASE "libaddress_sorting.so.9"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::address_sorting )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::address_sorting "${_IMPORT_PREFIX}/lib/libaddress_sorting.so.9.0.0" )
+
+# Import target "gRPC::gpr" for configuration "Release"
+set_property(TARGET gRPC::gpr APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::gpr PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libgpr.so.9.0.0"
+  IMPORTED_SONAME_RELEASE "libgpr.so.9"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::gpr )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::gpr "${_IMPORT_PREFIX}/lib/libgpr.so.9.0.0" )
+
+# Import target "gRPC::grpc" for configuration "Release"
+set_property(TARGET gRPC::grpc APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::grpc PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libgrpc.so.9.0.0"
+  IMPORTED_SONAME_RELEASE "libgrpc.so.9"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::grpc )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::grpc "${_IMPORT_PREFIX}/lib/libgrpc.so.9.0.0" )
+
+# Import target "gRPC::grpc_cronet" for configuration "Release"
+set_property(TARGET gRPC::grpc_cronet APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::grpc_cronet PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libgrpc_cronet.so.9.0.0"
+  IMPORTED_SONAME_RELEASE "libgrpc_cronet.so.9"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::grpc_cronet )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::grpc_cronet "${_IMPORT_PREFIX}/lib/libgrpc_cronet.so.9.0.0" )
+
+# Import target "gRPC::grpc_unsecure" for configuration "Release"
+set_property(TARGET gRPC::grpc_unsecure APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::grpc_unsecure PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libgrpc_unsecure.so.9.0.0"
+  IMPORTED_SONAME_RELEASE "libgrpc_unsecure.so.9"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::grpc_unsecure )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::grpc_unsecure "${_IMPORT_PREFIX}/lib/libgrpc_unsecure.so.9.0.0" )
+
+# Import target "gRPC::grpc++" for configuration "Release"
+set_property(TARGET gRPC::grpc++ APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::grpc++ PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libgrpc++.so.1.27.2"
+  IMPORTED_SONAME_RELEASE "libgrpc++.so.1"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::grpc++ )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::grpc++ "${_IMPORT_PREFIX}/lib/libgrpc++.so.1.27.2" )
+
+# Import target "gRPC::grpc++_alts" for configuration "Release"
+set_property(TARGET gRPC::grpc++_alts APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::grpc++_alts PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libgrpc++_alts.so.1.27.2"
+  IMPORTED_SONAME_RELEASE "libgrpc++_alts.so.1"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::grpc++_alts )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::grpc++_alts "${_IMPORT_PREFIX}/lib/libgrpc++_alts.so.1.27.2" )
+
+# Import target "gRPC::grpc++_error_details" for configuration "Release"
+set_property(TARGET gRPC::grpc++_error_details APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::grpc++_error_details PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libgrpc++_error_details.so.1.27.2"
+  IMPORTED_SONAME_RELEASE "libgrpc++_error_details.so.1"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::grpc++_error_details )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::grpc++_error_details "${_IMPORT_PREFIX}/lib/libgrpc++_error_details.so.1.27.2" )
+
+# Import target "gRPC::grpc++_reflection" for configuration "Release"
+set_property(TARGET gRPC::grpc++_reflection APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::grpc++_reflection PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libgrpc++_reflection.so.1.27.2"
+  IMPORTED_SONAME_RELEASE "libgrpc++_reflection.so.1"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::grpc++_reflection )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::grpc++_reflection "${_IMPORT_PREFIX}/lib/libgrpc++_reflection.so.1.27.2" )
+
+# Import target "gRPC::grpc++_unsecure" for configuration "Release"
+set_property(TARGET gRPC::grpc++_unsecure APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::grpc++_unsecure PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libgrpc++_unsecure.so.1.27.2"
+  IMPORTED_SONAME_RELEASE "libgrpc++_unsecure.so.1"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::grpc++_unsecure )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::grpc++_unsecure "${_IMPORT_PREFIX}/lib/libgrpc++_unsecure.so.1.27.2" )
+
+# Import target "gRPC::grpc_plugin_support" for configuration "Release"
+set_property(TARGET gRPC::grpc_plugin_support APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::grpc_plugin_support PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libgrpc_plugin_support.so.1.27.2"
+  IMPORTED_SONAME_RELEASE "libgrpc_plugin_support.so.1"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::grpc_plugin_support )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::grpc_plugin_support "${_IMPORT_PREFIX}/lib/libgrpc_plugin_support.so.1.27.2" )
+
+# Import target "gRPC::grpcpp_channelz" for configuration "Release"
+set_property(TARGET gRPC::grpcpp_channelz APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::grpcpp_channelz PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libgrpcpp_channelz.so.1.27.2"
+  IMPORTED_SONAME_RELEASE "libgrpcpp_channelz.so.1"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::grpcpp_channelz )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::grpcpp_channelz "${_IMPORT_PREFIX}/lib/libgrpcpp_channelz.so.1.27.2" )
+
+# Import target "gRPC::upb" for configuration "Release"
+set_property(TARGET gRPC::upb APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::upb PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libupb.so.9.0.0"
+  IMPORTED_SONAME_RELEASE "libupb.so.9"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::upb )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::upb "${_IMPORT_PREFIX}/lib/libupb.so.9.0.0" )
+
+# Import target "gRPC::grpc_cpp_plugin" for configuration "Release"
+set_property(TARGET gRPC::grpc_cpp_plugin APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::grpc_cpp_plugin PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/grpc_cpp_plugin"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::grpc_cpp_plugin )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::grpc_cpp_plugin "${_IMPORT_PREFIX}/bin/grpc_cpp_plugin" )
+
+# Import target "gRPC::grpc_csharp_plugin" for configuration "Release"
+set_property(TARGET gRPC::grpc_csharp_plugin APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::grpc_csharp_plugin PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/grpc_csharp_plugin"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::grpc_csharp_plugin )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::grpc_csharp_plugin "${_IMPORT_PREFIX}/bin/grpc_csharp_plugin" )
+
+# Import target "gRPC::grpc_node_plugin" for configuration "Release"
+set_property(TARGET gRPC::grpc_node_plugin APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::grpc_node_plugin PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/grpc_node_plugin"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::grpc_node_plugin )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::grpc_node_plugin "${_IMPORT_PREFIX}/bin/grpc_node_plugin" )
+
+# Import target "gRPC::grpc_objective_c_plugin" for configuration "Release"
+set_property(TARGET gRPC::grpc_objective_c_plugin APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::grpc_objective_c_plugin PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/grpc_objective_c_plugin"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::grpc_objective_c_plugin )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::grpc_objective_c_plugin "${_IMPORT_PREFIX}/bin/grpc_objective_c_plugin" )
+
+# Import target "gRPC::grpc_php_plugin" for configuration "Release"
+set_property(TARGET gRPC::grpc_php_plugin APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::grpc_php_plugin PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/grpc_php_plugin"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::grpc_php_plugin )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::grpc_php_plugin "${_IMPORT_PREFIX}/bin/grpc_php_plugin" )
+
+# Import target "gRPC::grpc_python_plugin" for configuration "Release"
+set_property(TARGET gRPC::grpc_python_plugin APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::grpc_python_plugin PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/grpc_python_plugin"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::grpc_python_plugin )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::grpc_python_plugin "${_IMPORT_PREFIX}/bin/grpc_python_plugin" )
+
+# Import target "gRPC::grpc_ruby_plugin" for configuration "Release"
+set_property(TARGET gRPC::grpc_ruby_plugin APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::grpc_ruby_plugin PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/grpc_ruby_plugin"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::grpc_ruby_plugin )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::grpc_ruby_plugin "${_IMPORT_PREFIX}/bin/grpc_ruby_plugin" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
